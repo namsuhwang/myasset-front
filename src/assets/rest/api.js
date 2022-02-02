593a5bfbcb8b7c04602cfc6dfa5093aafb5d0cac
황남수 <>
2022-02-02 15:45:34 +0900

    로그인

--- src/assets/rest/api.js
+++ src/assets/rest/api.js
@@ -30,15 +30,33 @@ api.interceptors.response.use(
   function (response) {
     console.log("api.interceptors.response.use confresponseig:" + JSON.stringify(response));
     // 응답이 정상(200)일 경우 호출
-    return response
+    return response;
   },
   function (error) {
     // 응답에 문제가 발생한(200을 제외한 나머지) 경우 호출
-    console.log("api.interceptors.response.use error : "  + error);
-    // window.location.href = "/system/login";
+    console.log("api.interceptors.response.use error : "  + JSON.stringify(error.response));
+    console.log("api.interceptors.response.use error.status : "  + error.response.status);
+  
+    if(error.response.status == 401 || error.response.status == 403){
+      router.push({name: 'Login', });
+    }else{
+      alert(error.message);
+    } 
     
-    router.push({name: 'Login', });
-    return error
+    /* 아래 코드는 참고용. 403 인 경우 리프레쉬 토큰이용하여 자동 재접속 해야 하는 경우임
+       https://minhyeong-jang.github.io/2020/01/08/js-axios-interceptors-error */
+    /*
+    if (error.response && error.response.status === 403) {
+      return Auth.refreshToken() // token 재발행 및 반환
+        .then(token => {
+          originalRequest.headers["Authorization"] = token;
+          return _axios.request(error.config); // error.config(origin API 정보)를 다시 요청
+        })
+        .catch(error => {
+          window.location.href = "/login";
+        });
+    }*/
+    return error;
   }
 )
 
